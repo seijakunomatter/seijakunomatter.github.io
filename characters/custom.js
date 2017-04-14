e63300634993cc50e39a9aa3c2b200a00a0ce0e5
Lucas Flicky @ Falk <lucasflicky@hotmail.com>
2017-04-14 20:06:19 -0300

    ch

---
+++ characters/custom.js
@@ -0,0 +1,97 @@
+
+         $(document).ready(function(){
+
+             var wid = $(window).width()
+             var amt = Math.floor(wid/200)
+
+
+               var slick = $('.container-nav').slick({
+                    asNavFor:".container-inner",
+                  waitForAnimate:false,
+    // centerMode:true,
+    //centerPadding:"0",
+      slidesToShow:22,
+      arrows:false,
+      initialSlide:0,
+      speed:300,
+                   infinite:false,
+      cssEase:'ease-out',
+      easing:'ease-out',
+      swipeToSlide:true,
+      focusOnSelect:true,
+    /*
+
+       responsive: [
+           {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 22,
+        slidesToScroll: 3,
+      }
+    },
+
+    {
+      breakpoint: 800,
+      settings: {
+         centerMode:true,
+           centerPadding:"50px",
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }}]
+      */
+               })
+
+  var slick = $('.container-inner').slick({
+   // setting-name: setting-value
+                    asNavFor:".container-nav",
+      waitForAnimate:false,
+     centerMode:true,
+    centerPadding:"210px",
+      slidesToShow:10,
+      arrows:false,
+      initialSlide:19,
+      speed:300,
+      cssEase:'ease-out',
+      easing:'ease-out',
+      swipeToSlide:true,
+      focusOnSelect:true,
+
+
+       responsive: [
+           {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 3,
+      }
+    },
+
+    {
+      breakpoint: 800,
+      settings: {
+         centerMode:true,
+           centerPadding:"50px",
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }],
+
+  });
+
+
+
+                 slick.on('mousewheel',function(e) {
+    e.preventDefault();
+
+    if (e.deltaY < 0) {
+      $(this).slick('slickNext');
+    }
+    else {
+      $(this).slick('slickPrev');
+    }
+  });
+
+
+});
+
+
